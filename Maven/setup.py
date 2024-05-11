d880ecc440582f8ba9b4ac36e9b93959ce668d39
AESWEYW <154776245+AESWEYW@users.noreply.github.com>
2024-05-10 19:12:02 -0700

    Initial commit of Maven project structure

---
+++ Maven/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='AI data gathering, AI data cleaning, AI training, AI pipeline, AI tools.',
+    author='Aug_ment',
+    license='MIT',
+)
